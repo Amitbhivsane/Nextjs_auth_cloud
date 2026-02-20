68fbd5391b0e1b5d4851e649950522a3ae11c7fe
Amit Bhivsane <amitbhivsane@gmail.com>
2026-02-20 12:27:58 +0530

    context

--- src/context/UserContext.tsx
+++ src/context/UserContext.tsx
@@ -1,46 +1,47 @@
 'use client'
 import axios from 'axios'
 import { useSession } from 'next-auth/react'
-import React, { useContext, useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
-type userContextType={
-user:userType | null | undefined,
-setUser:(user:userType)=>void
+type userType = {
+  name: string,
+  email: string,
+  id: string,
+  image?: string
 }
 
-type userType={
-  name:string,
-  email:string,
-  id:string,
-  image?:string 
+type userContextType = {
+  user: userType | null | undefined,
+  setUser: (user: userType) => void
 }
 
+export const userDataContext =
+  React.createContext<userContextType | undefined>(undefined)
 
-export const userDataContext=React.createContext<userContextType | undefined>(undefined)
+function UserContext({ children }: { children: React.ReactNode }) {
 
+  const { status } = useSession()
+  const [user, setUser] = useState<userType | null>()
 
-function UserContext({children}:{children:React.ReactNode}) {
-  const [user,setUser]=useState<userType | null>()
-  const session=useSession()
+  useEffect(() => {
 
-const data={
-  user,setUser
-}
+    async function getUser() {
+      try {
+        const result = await axios.get("/api/user")
+        setUser(result.data)
+      } catch (error) {
+        console.log("User fetch error:", error)
+      }
+    }
 
+    if (status === "authenticated") {
+      getUser()
+    }
 
-useEffect(()=>{
-async function getUser() {
-  try {
-    const result=await axios.get("/api/user")
-    setUser(result.data)
-  } catch (error) {
-    console.log(error)
-  }
-}
-getUser()
-},[session])
+  }, [status])
+
   return (
-    <userDataContext.Provider value={data}>
+    <userDataContext.Provider value={{ user, setUser }}>
       {children}
     </userDataContext.Provider>
   )
